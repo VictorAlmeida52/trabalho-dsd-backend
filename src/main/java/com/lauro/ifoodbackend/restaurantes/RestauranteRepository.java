911ccda4acb6b0ad0c8234312b6a3979c73bea39
Lauro Tomiatti <lauro.tomiatti@docente.unip.br>
2021-10-05 21:05:14 -0300

    adicao do projeto com restaurantes

---
+++ src/main/java/com/lauro/ifoodbackend/restaurantes/RestauranteRepository.java
@@ -0,0 +1,7 @@
+package com.lauro.ifoodbackend.restaurantes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+}
---
+++ src/test/java/com/lauro/ifoodbackend/IfoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauro.ifoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
